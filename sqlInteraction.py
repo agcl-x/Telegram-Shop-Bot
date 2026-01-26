ed552c457c79621fa62b54e626633ddaa3f1517d
agcl <romko.holets@gmail.com>
2026-01-26 17:30:50 +0200

    created sqlInteraction.py

---
+++ sqlInteraction.py
@@ -0,0 +1,20 @@
+from log import *
+
+def fetch_as_dicts(query, params=()):
+    log_sys(f"Initiating connection to database( {config["pathToDatabase"]} )")
+    with sqlite3.connect(config['pathToDatabase']) as conn:
+        log_sys("Successfully connected to database")
+        cur = conn.cursor()
+        cur.execute(query, params)
+        columns = [desc[0] for desc in cur.description]
+        log_sys("Data was successfully fetched")
+        return [dict(zip(columns, row)) for row in cur.fetchall()]
+
+def SQLmake(query, params=()):
+    log_sys(f"Initiating connection to database( {config["pathToDatabase"]} )")
+    with sqlite3.connect(config['pathToDatabase']) as conn:
+        log_sys("Successfully connected to database")
+        cur = conn.cursor()
+        cur.execute(query, params)
+        conn.commit()
+        return cur.lastrowid
