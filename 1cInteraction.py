5ed6a6bfa5fdebfcb0e5964c79049565c0522a61
agcl <romko.holets@gmail.com>
2026-01-26 13:54:58 +0200

    /

--- 1cInteraction.py
+++ 1cInteraction.py
@@ -1,9 +1,21 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+
 class Connection:
     def __init__(self):
-        pass
+        with open("config.json", "r") as file:
+            config = json.load(file)
+        # Ensure the URL ends correctly for the OData endpoint
+        self.base_url = config["ODataBaseUrl"].rstrip('/') + "/standard.odata"
+        self.auth = HTTPBasicAuth(config["1cUsername"], config["1cPassword"])
+        self.session = requests.Session()
+        self.session.auth = self.auth
 
-    def initiateConnection(self):
-        pass
+        self.session.headers.update({
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        })
 
     def closeConnection(self):
         pass
