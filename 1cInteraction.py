8b40a5c4e3ac080329120f9940bb49e18d4d20f9
agcl <romko.holets@gmail.com>
2026-01-26 14:29:59 +0200

    created initiateConnection func

--- 1cInteraction.py
+++ 1cInteraction.py
@@ -14,7 +14,13 @@ class Connection:
         self.initiateConnection()
 
     def initiateConnection(self):
-        pass
+        try:
+            connector = win32com.client.Dispatch("V83.COMConnector")
+            self.v8 = connector.Connect(self.connection_string)
+            print("Successfully connected to 1C on the local server.")
+        except Exception as e:
+            print(f"Failed to connect: {e}")
+            self.v8 = None
 
     def getNomenclature(self, s_nameIn="", s_articleIn=""):
         pass
