29602adde7baac8556982894be19757877e282a4
agcl <romko.holets@gmail.com>
2026-02-02 15:51:12 +0200

    added folder and file creation in log func

--- log.py
+++ log.py
@@ -1,8 +1,16 @@
+import os
 from datetime import datetime
 
+
 def log(user_id, message):
-    log_text = f"[{datetime.now().strftime("%H:%M %d.%m.%Y")}]\t{message}\n"
-    log_path = f"logs\\{user_id}.log"
+    folder_path = "logs"
+    log_path = os.path.join(folder_path, f"{user_id}.log")
+
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+    log_text = f"[{datetime.now().strftime('%H:%M %d.%m.%Y')}]\t{message}\n"
+
     with open(log_path, "a", encoding="utf-8") as f:
         f.write(log_text)
 
