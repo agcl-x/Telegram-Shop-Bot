843aafaae72594ddd7b29b5d67152b0b0afc3a4b
agcl <romko.holets@gmail.com>
2026-02-02 14:54:16 +0200

    /

--- main.py
+++ main.py
@@ -707,6 +707,7 @@ def formMessageText(article, user_id):
         log(user_id, f'[ERROR] Failed to form message for {article}: {e}')
         return "NULL"
 
+
     log(user_id, f'Start forming message for article: {article}')
 
     s_properties = ""
